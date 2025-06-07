c01c13e8829be8d4b6696b5cf6fec0689035b275
Azeddine Bedar <azzedinebedar@gamil.com>
2025-06-07 11:27:24 +0100

    Create Mobile Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
+// import Images
 import logo from "../assets/nav_img/logo.png";
 import cart2 from "../assets/nav_img/cart.svg";
+// import React Icons
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+// React hooks
+import { useState } from "react";
 
 const NavBar = () => {
+  // Toggle btn
+  const [isOpen, setIsOpen] = useState(false);
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-[var(--background-color)] p-4">
-      <div className="container mx-auto flex justify-around items-center">
+      {/* Desktop Navbar */}
+      <div className="hidden container mx-auto md:flex justify-around items-center">
         <div>
           <img src={logo} alt="Store logo" className="w-32" />
         </div>
@@ -30,6 +44,51 @@ const NavBar = () => {
             1
           </span>
           <img src={cart2} alt="Cart Icon" className="w-7 relative " />
+        </div>
+      </div>
+
+      {/* Mobile Navbar */}
+      <div className="container mx-auto md:hidden flex justify-between items-center">
+        {/* Hamburger Menu Icon */}
+        {!isOpen ? (
+          <RxHamburgerMenu className="text-3xl" onClick={toggleMenu} />
+        ) : (
+          <IoMdClose className="text-3xl" onClick={toggleMenu} />
+        )}
+
+        <div>
+          <img src={logo} alt="Store logo" className="w-32" />
+        </div>
+        <div className="relative cursor-pointer">
+          <span className="absolute top-[-8px] right-[-4px] text-[var(--background-color)] bg-[var(--primary-color)] rounded-full w-4 h-4 flex items-center justify-center text-xs font-semibold z-20">
+            1
+          </span>
+          <img src={cart2} alt="Cart Icon" className="w-7 relative " />
+        </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={`overflow-hidden transition-all duration-300 ${
+            isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+          } absolute top-16 left-0 right-0 z-50 bg-[var(--background-color)] shadow-md rounded-lg`}
+        >
+          <ul className="flex flex-col space-y-2 px-5 py-4 text-lg">
+            <li>
+              <a href="/" className="navbar-link">
+                Discovery
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="navbar-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="navbar-link">
+                Contact us
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
